32037340673fafda04a846b8c83ebccb709ccb80
Jesse Väärälä <jesse.vaarala@gmail.com>
2020-09-30 15:25:50 +0300

    pelilogiikkaa

--- front/src/components/Quiz.vue
+++ front/src/components/Quiz.vue
@@ -1,21 +1,22 @@
 <template>
-    <div>
-      Quiz
-      <h2>{{decodeHtml(this.content.results[0].question)}}</h2>
-      <b-form-group label="">
-        <b-form-radio v-model="selected" name="some-radios" value="A">
-          {{decodeHtml(this.content.results[0].correct_answer)}}
-        </b-form-radio>
-        <b-form-radio v-model="selected" name="some-radios" value="B">
-          {{decodeHtml(this.content.results[0].incorrect_answers[0])}}
-        </b-form-radio>
-      </b-form-group>
-
-      <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+  <div>
+    Quiz
+    <h2>{{decodeHtml(this.content.results[0].question)}}</h2>
+    <div class="btn-group-vertical">
+      <b-button style="margin: 10px 0 10px 0"
+                v-for="answer in answers" :key="answer" v-on:click="handleAnswerButton">
+        {{decodeHtml(answer)}}
+      </b-button>
     </div>
+    <div>
+      {{ points }}
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'Quiz',
   data() {
@@ -30,23 +31,39 @@ export default {
       txt.innerHTML = html;
       return txt.value;
     },
+    handleAnswerButton(event) {
+      console.log(event);
+      if (event.target.innerText === this.content.results[0].correct_answer) {
+        console.log('Oikea vastaus');
+        this.$store.commit('INCREMENT_POINTS', 1);
+        this.fetchQuestion();
+      } else {
+        console.log('Väärä vastaus');
+        this.$store.commit('RESET_POINTS');
+        this.$store.commit('TOGGLE_GAMEON', false);
+      }
+    },
+    fetchQuestion() {
+      const myHeaders = new Headers();
+      myHeaders.append('Cookie', 'PHPSESSID=0ed9a39e9f8b1f8b4671ad784fcd582b');
+
+      const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'manual',
+      };
+      fetch('https://opentdb.com/api.php?amount=1&category=18',
+        requestOptions).then((response) => response.json()).then((result) => {
+        this.content = result;
+        this.answers = this.content.results[0].incorrect_answers;
+        this.answers.push(this.content.results[0].correct_answer);
+      }).catch((error) => console.log('error', error));
+    },
   },
   mounted() {
-    const myHeaders = new Headers();
-    myHeaders.append('Cookie', 'PHPSESSID=0ed9a39e9f8b1f8b4671ad784fcd582b');
-
-    const requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'manual',
-    };
-    fetch('https://opentdb.com/api.php?amount=1&category=18',
-      requestOptions).then((response) => response.json()).then((result) => {
-      this.content = result;
-      this.answers = this.content.results[0].incorrect_answers;
-      this.answers.push(this.content.results[0].correct_answer);
-    }).catch((error) => console.log('error', error));
+    this.fetchQuestion();
   },
+  computed: mapState(['gameOn', 'user', 'points']),
 };
 </script>
 
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -11,11 +11,13 @@ export default new Vuex.Store({
     },
     users: [],
     gameOn: true,
+    points: 0,
   },
   getters: {
     user: (state) => state.user,
     users: (state) => state.users,
     gameOn: (state) => state.gameOn,
+    points: (state) => state.points,
   },
   mutations: {
     FILL_USER(state, user) {
@@ -26,6 +28,12 @@ export default new Vuex.Store({
     },
     TOGGLE_GAMEON(state, gameOn) {
       state.gameOn = gameOn;
+    },
+    INCREMENT_POINTS(state, n) {
+      state.points += n;
+    },
+    RESET_POINTS(state) {
+      state.points = 0;
     },
   },
   actions: {
